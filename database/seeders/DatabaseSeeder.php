4ffc7fb3c79c4a87ae108d8e6aee7a87cf152b08
Rafael Castillo <rafaelcastillo@outlook.com>
2020-12-08 16:42:04 -0500

    Modelos nuevos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Area;
+use App\Models\Instructor;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +16,22 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        Instructor::create(['name' => 'Daniel Susaeta', 'mnemonic' => 'DS']);
+        Instructor::create(['name' => 'Jorge Monckeberg', 'mnemonic' => 'JMB']);
+        Instructor::create(['name' => 'Josemaría Vázquez', 'mnemonic' => 'JV']);
+        Instructor::create(['name' => 'Roberto Estrada', 'mnemonic' => 'RE']);
+        Instructor::create(['name' => 'Abel Defina', 'mnemonic' => 'AD']);
+        Instructor::create(['name' => 'Raúl Moncayo', 'mnemonic' => 'RM']);
+        Instructor::create(['name' => 'Antonio Villasís', 'mnemonic' => 'AV']);
+        Instructor::create(['name' => 'Patricio Vergara', 'mnemonic' => 'PV']);
+        Instructor::create(['name' => 'José Aulestia', 'mnemonic' => 'JA']);
+
+        Area::create(['name' => 'Dirección de Operaciones', 'mnemonic' => 'OPE']);
+        Area::create(['name' => 'Dirección Financiera', 'mnemonic' => 'FIN']);
+        Area::create(['name' => 'Dirección Estratégica', 'mnemonic' => 'DIRE']);
+        Area::create(['name' => 'Dirección de Personas', 'mnemonic' => 'DP']);
+        Area::create(['name' => 'Dirección Comercial', 'mnemonic' => 'COM']);
+        Area::create(['name' => 'Análisis de Decisiones', 'mnemonic' => 'ADD']);
+        Area::create(['name' => 'Sistemas de Dirección y Control', 'mnemonic' => 'SDC']);
     }
 }
