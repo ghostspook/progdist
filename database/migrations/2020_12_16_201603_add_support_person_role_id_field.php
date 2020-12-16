4f03fac844583847708e04b3faafeec45f1a79ec
Rafael Castillo <rafaelcastillo@outlook.com>
2020-12-16 15:20:39 -0500

    New support_person_roles

---
+++ database/migrations/2020_12_16_201603_add_support_person_role_id_field.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddSupportPersonRoleIdField extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('support_persons', function (Blueprint $table) {
+            $table->foreignId('support_person_role_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('support_persons', function (Blueprint $table) {
+            $table->dropForeign(['support_person_role_id']);
+            $table->dropColumn('support_person_role_id');
+        });
+    }
+}
