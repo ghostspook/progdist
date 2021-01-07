f90b0db2dbb7fe95998327234cad44fd0df704ce
sabbih777 <xdyerm@gmail.com>
2021-01-07 14:29:53 -0500

    Checkin en máquina Xavier

--- public/js/app.js
+++ public/js/app.js
@@ -515,8 +515,8 @@ eval("// removed by extract-text-webpack-plugin//# sourceURL=[module]\n//# sourc
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/vagrant/code/progdist/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/vagrant/code/progdist/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\xdyer\Laravel\proyectos\progdist\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\xdyer\Laravel\proyectos\progdist\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
