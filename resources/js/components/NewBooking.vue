5990dd771f9399d5d8ffd4364e2557c6dfcdc747
sabbih777 <xdyerm@gmail.com>
2021-01-14 14:51:08 -0500

    Fila / columna / form bootstrap

--- resources/js/components/NewBooking.vue
+++ resources/js/components/NewBooking.vue
@@ -1,8 +1,20 @@
 <template>
     <div class="ml-2 mr-2">
         <button class="btn btn-primary" @click="onNewClick">{{ newButtonText }}</button>
-        <div v-if="displayForm" class="mt-1 mb-2">
-            Aquí va el formulario
+        <div v-if="displayForm" class="mt-2 mb-3">
+            <form>
+                <div class="row">
+                        <div class="col-md-2 form-group">
+                            <label for="subject">Tema</label>
+                            <input type="text" id="subject" class="form-control"/>
+                        </div>
+                        <div class="col-md-2">Columna 2</div>
+                        <div class="col-md-2">Columna 3</div>
+                        <div class="col-md-2">Columna 4</div>
+                        <div class="col-md-2">Columna 5</div>
+                        <div class="col-md-2">Columna 6</div>
+                </div>
+            </form>
         </div>
     </div>
 </template>
