e77ed75f09d44896506951b363ea3bc164085a52
sabbih777 <75511841+sabbih777@users.noreply.github.com>
2021-04-28 14:23:40 -0500

    Mark unmark dates for cloning

--- resources/js/components/BookingClone.vue
+++ resources/js/components/BookingClone.vue
@@ -5,7 +5,8 @@
         </h5>
         <div class="card-body">
             <div class="row justify-content-center">
-                <div class="my-calendar">
+                <div class="row">
+                <div class="my-calendar col-md-6" >
                     <vue-cal
                         hide-view-selector
                         :time="false"
@@ -16,6 +17,22 @@
                     >
                     </vue-cal>
                 </div>
+                    <div class="col-md-6">
+                        <font-awesome-icon  icon="calendar-day"/>
+                            Fechas Seleccionadas
+                            <multiselect
+                                v-model="events"
+                                :options="events"
+                                track-by="label"
+                                label="label"
+                                :multiple="true"
+                                :taggable="true"
+                                :showLabels="true"
+                                :hide-selected="true"
+                            ></multiselect>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
@@ -23,11 +40,13 @@
 
 <script>
 import VueCal from 'vue-cal'
+import Multiselect from "vue-multiselect";
 import moment from 'moment'
 
 export default {
     components: {
-        VueCal
+        VueCal,
+        Multiselect
     },
     props: {
         booking: {
@@ -40,11 +59,38 @@ export default {
             events: []
         }
     },
+    computed: {
+
+        selectedDates() {
+            var selectedDates = []
+            this.events.array.forEach(element => {
+
+            });
+
+            return this.events.map((event) => moment(event.start).format("DD/MM/YYYY"));
+        },
+    },
     methods: {
         onCellClick (e) {
-            console.log(e)
-            this.events.push({ start: moment(e).startOf('day').toDate(), end: moment(e).endOf('day').toDate(), title: this.booking.program.mnemonic })
+
+            var eventsThisDay = this.events.filter( (ev) =>
+                        moment(e).startOf('day').isSame(ev.start, "day")
+                    );
+
+            if (eventsThisDay.length == 0) {
+                this.events.push({
+                    start: moment(e).startOf('day').toDate(),
+                    end: moment(e).endOf('day').toDate(),
+                    title: this.booking.program.mnemonic,
+                    label: moment(e).format("DD/MM/YYYY"),
+                })
+            } else {
+                this.events = this.events.filter(evt => evt !== eventsThisDay[0])
+
+            }
+
         }
+
     }
 }
 </script>
